4a37eecc6165144d87e6f8bce8261f954e195dcc
Busisiwe Makunga <38431312+khanyokuhle@users.noreply.github.com>
2023-11-12 00:17:36 +0200

    moved gitignore

--- cypress-automation/cypress.config.js
+++ cypress-automation/cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Self Development Project',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: "https://react-redux.realworld.io",
     viewportHeight: 700,
